24cc32f2b8dab365a70d8fdd4881bdec79984fc3
Orest0501 <oborshovskiy@gmail.com>
2022-06-20 13:06:26 +0300

    lab11 was edited

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ def main():
 
 
 with open("access.log.txt", "r") as zip_file:
-    logs_file = zip_file.read().split("\n")
 
-    allRequest = re.findall(".*\[23/Mar/2009:0[2-5]:[0-5][0-9]:[0-9][0-9] \+0100].*php.* HTTP/1\.[0-9]\" 3[0-9][0-9].*",
-                            str(logs_file))
-    print(len(allRequest))
+    RE_PATTERN = re.compile(
+        ".*\[23/Mar/2009:0[2-5]:[0-5][0-9]:[0-9][0-9] \+0100].*php.* HTTP/1\.[0-9]\" 3[0-9][0-9].*")
+
+    if __name__ == "__main__":
+        qty = 0
+
+        for line in zip_file.readlines():
+            matcher = RE_PATTERN.findall(line)
+            qty += len(matcher)
+    print("Result: {}".format(qty))
 
 
